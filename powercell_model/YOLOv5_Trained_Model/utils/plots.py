1d2c98795915e6a592aafce7939b2312246de381
Ryan Bode <rbcrusher88@gmail.com>
2021-01-27 22:28:15 -0500

    x, y, distance calculations

--- powercell_model/YOLOv5_Trained_Model/utils/plots.py
+++ powercell_model/YOLOv5_Trained_Model/utils/plots.py
@@ -57,6 +57,14 @@ def plot_one_box(x, img, color=None, label=None, line_thickness=None):
     tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
     color = color or [random.randint(0, 255) for _ in range(3)]
     c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
+    width = int(x[0]) - int(x[2])
+    height = int(x[1]) - int(x[3])
+    ty = int(x[3]) + int(height / 2)
+    cameraHeight = 10 # TODO Change this value to match actual height on the bot
+    cameraAngel = 90 # TODO Change this value to match actual angel on the bot
+    distance = (3.5-cameraHeight)*(1/math.tan(math.radians(cameraAngel+ty)))
+    print(distance)
+    cv2.circle(img, (int(x[2]) + int(width / 2),int(x[3]) + int(height / 2)), 10, [0,0,255], -1)
     cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
     if label:
         tf = max(tl - 1, 1)  # font thickness
