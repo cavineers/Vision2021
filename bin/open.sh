3aef11b07ed1b47ecd60c096f0b58988d1c4fc0a
Ryan Bode <rbcrusher88@gmail.com>
2021-03-02 23:58:54 -0500

    fix: updated open-u bash scripts

--- bin/open.sh
+++ bin/open.sh
@@ -1,7 +1,3 @@
-#!/bin/bash  
-echo "Running Script." 
-sudo docker run -it --device=/dev/video0 --runtime nvidia --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" --rm yolov5-cavs
-git pull
-cd Vision2021/powercell_model/YOLOv5_Trained_Model
-echo "Starting detect.py"
-python3 detect.py --weights ./best.pt --source 0
+#!/bin/bash
+echo "Running Script."
+sudo docker run -it --name vision2021 --device=/dev/video0 --runtime nvidia --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" --rm yolov5-cavs bash -c "cd Vision2021/powercell_model/YOLOv5_Trained_Model && git pull && python3 detect.py --weights ./best.pt --source 0"
