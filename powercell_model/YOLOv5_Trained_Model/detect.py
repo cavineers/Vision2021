7bf220a2951e1f72a0230e104e6e2f54d7a1aa2e
Ryan Bode <rbcrusher88@gmail.com>
2021-03-20 21:01:48 -0400

    test: take 4

--- powercell_model/YOLOv5_Trained_Model/detect.py
+++ powercell_model/YOLOv5_Trained_Model/detect.py
@@ -241,6 +241,7 @@ def connectSockets():
         ws.on_open = detect
         ws.run_forever()
     else:
+        print(opt.dev)
         uri = opt.dev
         ws = websocket.WebSocketApp(uri, on_open = on_open, on_error = on_error, on_close = on_close)
         ws.on_open = detect
