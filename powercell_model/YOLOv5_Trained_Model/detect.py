6adfa6e92d33781eae892ba0efd6648986ebb0e3
Ryan Bode <rbcrusher88@gmail.com>
2021-03-26 08:32:54 -0400

    fix: recursion issue with websocket connections

--- powercell_model/YOLOv5_Trained_Model/detect.py
+++ powercell_model/YOLOv5_Trained_Model/detect.py
@@ -224,44 +224,50 @@ if __name__ == '__main__':
 
 ws = undefined
 
-def on_close(ws, test):
-    global first, dataset
-    print("Connection to socket closed... Attempting Reconnect.")
-    first=False
-    dataset.destroy()
-    time.sleep(1)
-    connectSockets()
+# def on_close(ws, test):
+#     global first, dataset
+#     print("Connection to socket closed... Attempting Reconnect.")
+#     first=False
+#     dataset.destroy()
+#     time.sleep(1)
+#     connectSockets()
 
-def on_open(ws, test):
-    print("Connection Established")
-
-def on_error(ws, test):
-    global first, dataset
-    print("Connection to socket failed... Attempting Reconnect.")
-    first=False
-    dataset.destroy()
-    time.sleep(1)
-    connectSockets()
+# def on_error(ws, test):
+#     global first, dataset
+#     print("Connection to socket failed... Attempting Reconnect.")
+#     first=False
+#     dataset.destroy()
+#     time.sleep(1)
+#     connectSockets()
 
 def connectSockets():
     print("Attempting Initial Websocket Connection")
     if opt.dev == False:
         uri = "ws://10.45.41.2:5808"
-        ws = websocket.WebSocketApp(uri, on_open = on_open, on_error = on_error, on_close = on_close)
-        ws.on_open = detect
-        ws.run_forever()
+        ws = websocket.WebSocketApp(uri, on_open = detect) #on_error = on_error, on_close = on_close
+        while True:
+            try:
+                ws.run_forever()
+            except:
+                pass
     elif opt.dev == "na":
         detect("na")
     elif opt.dev == "t":
         uri = "ws://localhost:5808"
-        ws = websocket.WebSocketApp(uri, on_open = on_open, on_error = on_error, on_close = on_close)
-        ws.on_open = detect
-        ws.run_forever()
+        ws = websocket.WebSocketApp(uri, on_open = detect) #on_error = on_error, on_close = on_close
+        while True:
+            try:
+                ws.run_forever()
+            except:
+                pass
     else:
         print(opt.dev)
         uri = opt.dev
-        ws = websocket.WebSocketApp(uri, on_open = on_open, on_error = on_error, on_close = on_close)
-        ws.on_open = detect
-        ws.run_forever()
+        ws = websocket.WebSocketApp(uri, on_open = detect) #on_error = on_error, on_close = on_close
+        while True:
+            try:
+                ws.run_forever()
+            except:
+                pass
 
 connectSockets()
